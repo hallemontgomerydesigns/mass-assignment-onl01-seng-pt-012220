0c758b283808558e772111f811fec6b140fca0fb
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-03-05 00:13:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(attributes=nil)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    if attributes 
   end
 end
